b37a38a8cc4abc1d7054c2d4f0d7b239acea2e79
azaa1 <azaa.chi11@gmail.com>
2021-01-16 12:46:09 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+### Links 
+
+## HTML, SCSS, JS
+https://codepen.io/RobinTreur/pen/pyWLeB
+
+## Flask Login
+https://www.digitalocean.com/community/tutorials/how-to-add-authentication-to-your-app-with-flask-login
---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, redirect, url_for, request
+from datetime import datetime
+
+app = Flask(__name__)
+
+# Render index.html
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+# Render about.html
+@app.route('/about')
+def about():
+   return render_template('about.html')
+
+# datetime
+@app.context_processor
+def inject_now():
+    return {'now': datetime.utcnow()}
+
+
+       
+if __name__ == '__main__':
+   app.run(debug = True)
